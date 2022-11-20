a7477f39cc5cb9c2f8d25d6e3cbd2a48f1f07817
Tommy <baseballplayer987@hotmail.com>
2022-11-20 17:27:04 -0500

    deliverables complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,19 @@
-library(dbplyr)
+# library
+library(dplyr)
+# Linear Regression to Predict MPG
 mechacar <- read.csv('MechaCar_mpg.csv')
 model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar)
 summary(model)
+# Summary Statistics on Suspension Coils
+suspensioncoil <- read.csv('Suspension_Coil.csv')
+total_summary <- suspensioncoil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- suspensioncoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+# T-Tests on Suspension Coils
+t.test(x=suspensioncoil$PSI, mu=1500)
+subset1 <- subset(suspensioncoil, Manufacturing_Lot=='Lot1')
+subset2 <- subset(suspensioncoil, Manufacturing_Lot=='Lot2')
+subset3 <- subset(suspensioncoil, Manufacturing_Lot=='Lot3')
+t.test(x=subset1$PSI, mu=1500)
+t.test(x=subset2$PSI, mu=1500)
+t.test(x=subset3$PSI, mu=1500)
+
